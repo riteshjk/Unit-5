4b03ec3b8b44ad41380912251e1068214e57bc9c
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-03-09 22:49:43 +0530

    new

--- sprint-2/day-1/todolist/src/components/Grocery.jsx
+++ sprint-2/day-1/todolist/src/components/Grocery.jsx
@@ -1,18 +1,44 @@
 import {GroceryInput} from "./GroceryInput"
-import {useState} from "react"
+import {useState,useEffect} from "react"
 import {GroceryList} from "./GroceryList"
 import { nanoid } from 'nanoid'
+const axios = require('axios');
 
 
 
 export function Grocery(){
     const [gros ,setGros ] = useState([])
+    const [page , setPage] = useState(1)
     
     const addGrocery =(data)=>{
         console.log(data)
         const payload = {id:nanoid(), title:data}
         setGros([...gros,payload])
         console.log(data)
+
+
+        axios.post("http://localhost:3001/grocery",payload).then(function(response){
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            console.log(error);
+          })
+
+
+    }
+
+    useEffect(()=>{
+       showData();
+    },[page])
+
+    function showData(){
+        axios.get(`http://localhost:3001/grocery?_limit=3&_page=${page}`).then(function(response){
+            //console.log(response.data)
+            setGros(response.data)
+        })
+        .catch(function (error) {
+            console.log(error);
+          })
     }
 
     const deleteGrocery=(id)=>{
@@ -27,6 +53,9 @@ export function Grocery(){
         {gros.map((e)=>{
            return <GroceryList value ={e} deleteGrocery={deleteGrocery}/>
         })}
+        <button onClick={()=>setPage(page+1)}>Next</button>
+        <button onClick={()=>setPage(page-1)}>Prev</button>
+
     </div>
     );
 }
