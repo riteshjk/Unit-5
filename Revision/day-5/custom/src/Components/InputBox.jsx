ffafe750675cd5e87fce54a96ceccc1eddf2da32
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-04-19 01:36:19 +0530

    very few changes required

---
+++ Revision/day-5/custom/src/Components/InputBox.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import { useState,useRef,useEffect } from 'react';
+import { PinItem } from './PinItem';
+
+export const InputBox = ({length,label, perBox,onChange})=>{
+const [boxe ,setBox] = useState(new Array(length).fill(""));
+const element=useRef(new Array(length).fill(0));
+
+const handlechange=(box,index)=>{
+    console.log(box,index)
+    const value=[...boxe];
+    value[index]=box;
+    setBox(value);
+    if(box.length > 0 && box.length <= perBox && index<length-1){
+        element.current[index+1].focus();
+    }
+   onChange(value.join(""))
+       
+    }
+
+    const handleBackspace=(box,index)=>{
+        if(index>0){
+            element.current[index-1].focus();
+        }
+        const value=[...boxe];
+        value[index]=box;
+        setBox(value);
+        onChange(value.join(""))
+    }
+
+useEffect(()=>{
+    console.log(element.current)
+},[])
+
+    return (
+        <div>
+            <h1>{label}</h1>
+        {boxe.map((item,index)=><PinItem key={index} ref={(n)=>(element.current[index]=n)} onChange={(b)=>handlechange(b,index)} onBackspace={(b)=>handleBackspace(b,index)} max={perBox}/>)}
+       
+        </div>
+
+    )
+
+}
+
+InputBox.propTypes = {
+    length: PropTypes.number.isRequired,
+    perBox: PropTypes.number,
+    label: PropTypes.string
+}
+
+InputBox.defaultProps = {
+    label: 'Label',
+    perBox: 3
+}
---
+++ Revision/day-5/custom/src/Components/PinItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+const style ={
+    padding: 10,
+    margin: 10,
+    width: 10,
+    fontSize: 10,
+}
+
+
+export const PinItem=React.forwardRef(({onChange,onBackspace,max},ref)=>{
+
+    const handlevalue=(e)=>{
+        console.log(e.keyCode)
+        switch(e.keyCode){
+            case 8:{
+                if(e.target.value) onBackspace(e.target)
+                
+                break;
+
+            }
+            case 9:{
+                e.preventDefault();
+                break;
+            }
+            default:{   
+                onChange(e.target.value)
+           
+            }
+
+        }
+    }
+
+    return(
+       <input onKeyUp={handlevalue} ref={ref} style={style} maxlength={max}/>
+    )
+})
